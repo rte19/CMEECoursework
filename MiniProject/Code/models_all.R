b26b16a10cb0ea40373a9ec5d3f69ab2a703d8e3
Ryan Ellis <ryan.ellis19@imperial.ac.uk>
2020-02-18 22:32:30 +0000

    Logistic fit 125 times, Gompertz fits none

--- MiniProject/Code/models_all.R
+++ MiniProject/Code/models_all.R
@@ -11,6 +11,7 @@ library(minpack.lm)
 Data <- read.csv("../Data/LogisticGrowthData.csv", header = TRUE) #Reading in the original data
 Data$Temp <- as.factor(Data$Temp) #Making the temperature values factors - categorical instead of continuous
 
+Data <- Data[-c(which(Data$PopBio == -668.283935900778)), ]
 ###Nesting the data for each unique combination of species, medium and temp, to then run models on each of these dataframes
 Data2 <- Data %>%
   nest(-Temp,-Medium,-Species) #Using tidyr to create loads of dataframes for each combination of Temp, Medium, Species
@@ -36,8 +37,21 @@ LogisticMod <- function(N0, Nmax, r, t){
 # r = maximum growth rate (rmax)
 # t = time
 
-###Creating the variables to hold all stats results
-StatsRESULTS <- as.data.frame(matrix(nrow = 1, ncol = 9))
+###Defining the Gompertz model
+GompertzMod <- function(N0, Nmax, r, t, A, tlag){
+  return(A * exp(-exp(((rmax * exp(tlag - t)) / A) + 1)))
+}
+
+# Nt = population size at time t, PopBio
+# N0 = initial population size
+# Nmax = carrying capacity (K)
+# r = maximum growth rate (rmax)
+# t = time
+# A = the asymptote A = log(Nmax/N0)
+# tlag = is the x-axis intercept to this tangent (duration of the delay before the popuation starts growing exponentially)
+
+###Creating the dataframe to hold all stats results
+StatsRESULTS <- data.frame(ID = character(), Straight_Line_AIC = numeric(), Quadrtic_AIC = numeric(), Cubic_AIC = numeric(), Logistic_AIC = numeric(), Gompertz_AIC = numeric(), stringsAsFactors = FALSE)
 
 ###Fitting Linear models and recording RSq, AIC and BIC for all models for each ID
 
@@ -46,66 +60,60 @@ for (i in 1:length(Data2$data)){
   ModelData <- Data2$data[[i]] #Getting one set of data at a time for each of the models
   
   N0 <- min(ModelData$PopBio) #Starting value for N0 being the mininmum of the PopBio data
-  Nmax <- max(ModelData$PopBio) #Starting value for Nmax being the maximum of the PopBio data
-  
-  # ModelDataMax <- dplyr::filter(ModelData, PopBio == max(ModelData$PopBio)) #Extracting the row with the maximum PopBio
-  # ModelDataMaxTime <- as.numeric(as.data.frame(ModelDataMax[1,2])) #Obtaining the Time of the maximum PopBio
-  # rData <- dplyr::filter(ModelData, Time <= ModelDataMaxTime) #Extracting the rows all with Times <= the Time of the maximum PopBio
-  # rDataFit <- lm(rData$PopBio ~ rData$Time) #Running a straight line through all these data points at and before the maximum PopBio
-  # r <- rDataFit$coefficients[2] #Starting value of r being the gradient of these extracted data points
+  Nmax <- max(ModelData$PopBio) #Starting value for Nmax being the maximum of the PopBio data 
   
   rdata <- ModelData
   a <- lm(rdata$PopBio ~ rdata$Time)
   r <- a$coefficients[2]
-  for(i in 1:(dim(ModelData)[1] - 2)){
+  for(j in 1:(dim(ModelData)[1] - 2)){
     rdata <- rdata[-c(which(ModelData$Time == max(ModelData$Time))), ]
-    a2 <- lm(rdata$PopBio ~ rdata$Time)
-    r2 <- a2$coefficients[2]
-    if(r < r2){
-      r <- r2
+    if(length(unique(rdata$Time)) != 1){
+      a2 <- lm(rdata$PopBio ~ rdata$Time)
+      r2 <- a2$coefficients[2]
+      if(r < r2){
+        r <- r2
+      }
     }
+    # if (r > 0.9) {
+    #   break
+    # }
   }
   r <- as.numeric(r)
   
+  A <- log(Nmax / N0)
+  tlag <- min(ModelData$PopBio)
+  
   ID <- Data2$ID[i] #Getting the ID for each dataset modelled
   StraiFit <- lm(PopBio ~ Time, data = ModelData) #Running the straight line model  
-  StraiModel <- "StraightLine" #Defining the name of the model
-#  StraiRSq <- RSqfun(StraiFit, ModelData$PopBio) #Calling the RSq of the straight line model
   StraiAIC <- AIC(StraiFit) #Calling the AIC of the straight line model
-#  StraiBIC <- BIC(StraiFit) #Calling the BIC of the straight line model
   
   QuaFit <- lm(PopBio ~ poly(Time, 2), data = ModelData) #Runing the quadratic model    
-  QuaModel <- "Quadratic" #Calling the model name, RSq, AIC and BIC of the quadratic linear model
-#  QuaRSq <- RSqfun(QuaFit, ModelData$PopBio)
   QuaiAIC <- AIC(QuaFit)
-#  QuaBIC <- BIC(QuaFit)
   
   CubFit <- lm(PopBio ~ poly(Time, 3), data = ModelData) #Running the cubic model    
-  CubModel <- "Cubic" #Calling the model name, RSq, AIC and BIC of the cubic linear model
-#  CubRSq <- RSqfun(CubFit, ModelData$PopBio)
   CubAIC <- AIC(CubFit)
-#  CubBIC <- BIC(CubFit)
   
-  LogisFit <- try(nlsLM(PopBio ~ LogisticMod(N0, Nmax, r, t = Time), data = ModelData, start = c(N0 = N0, Nmax = Nmax, r = r)), silent = T) #Running the logistic model with prior estimated starting values, but using try to keep going when it fails to optimise 
-  LogisModel <- "Logistic"
+  LogisFit <- try(nlsLM(PopBio ~ LogisticMod(N0, Nmax, r, t = Time), data = ModelData, start = c(N0 = N0, Nmax = Nmax, r = r), control = nls.lm.control(maxiter = 100)), silent = T) #Running the logistic model with prior estimated starting values, but using try to keep going when it fails to optimise
   if (class(LogisFit) != 'try-error'){
-#    LogisRSq <- RSqfun(LogisFit, ModelData$PopBio)
     LogisAIC <- AIC(LogisFit)
-#    LogisBIC <- BIC(LogisFit)
   } else {
-#    LogisRSq <- "NA"
     LogisAIC <- "NA"
-#    LogisBIC <- "NA"
   }
   
-#  temp.vector <- c(ID, StraiModel, StraiRSq, StraiAIC, StraiBIC, QuaModel, QuaRSq, QuaiAIC, QuaBIC, CubModel, CubRSq, CubAIC, CubBIC, LogisModel, LogisRSq, LogisAIC, LogisBIC)
-  temp.vector <- c(ID, StraiModel, StraiAIC, QuaModel, QuaiAIC, CubModel, CubAIC, LogisModel, LogisAIC)
+  GompertzFit <- try(nlsLM(PopBio ~ GompertzMod(N0, Nmax, r, t = Time, A, tlag), data = ModelData, start = c(N0 = N0, Nmax = Nmax, r = r, A = A, tlag = tlag), control = nls.lm.control(maxiter = 100)), silent = T) #Running the Gompertz model with prior estimated starting values, but using try to keep going when it fails to optimise
+  if (class(GompertzFit) != 'try-error'){
+    GompertzAIC <- AIC(GompertzFit)
+  } else {
+    GompertzAIC <- "NA"
+  }
   
-  StatsRESULTS <- rbind(StatsRESULTS, temp.vector)
+  temp.vector <- c(ID, StraiAIC, QuaiAIC, CubAIC, LogisAIC, GompertzAIC)
+  
+  StatsRESULTS[i, ] <- temp.vector
 }
-#names(StatsRESULTS) <- c("ID", "Model_Name", "RSq", "AIC", "BIC", "Model_Name", "RSq", "AIC", "BIC", "Model_Name", "RSq", "AIC", "BIC", "Model_Name", "RSq", "AIC", "BIC")
-names(StatsRESULTS) <- c("ID", "Model_Name", "AIC", "Model_Name", "AIC", "Model_Name", "AIC", "Model_Name", "AIC")
 
+counter <- 0
+for(i in StatsRESULTS$Logistic_AIC){ if(i != "NA"){counter <- counter + 1}}
 
 #For both AIC and BIC, If model A has AIC lower by 2-3 or more than
 #model B, it’s better — Differences of less than 2-3 don’t really matter
